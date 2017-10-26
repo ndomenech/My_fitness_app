f7130f8e0b02963171b14a9eda1f41604be2d74f
Nicholas Domenech <domenecn1@hawkmail.newpaltz.edu>
2017-10-26 14:04:31 -0400

    jquery fix add actitty

--- jquery-mockup/addActivity.js
+++ jquery-mockup/addActivity.js
@@ -1,18 +1,12 @@
-
-function addActivity() {
-    var list = document.getElementById('activityList');
-
-    var activityObj = document.getElementsById("userExercise")
-    var activity = activityObj.value
-    var amountObj = document.getElementsById("userAmount")
-    var amount = amountObj.value
-    
-console.log("User input"+activity+" , "+amount);
-
-    var entry = document.createElement('li');
-
-    entry.appendChild(document.createTextNode(activity+" , "+amount));
-    list.appendChild(entry);
-    
-}
-
+$(function(){
+            
+        $("#actList-placeholder").load("_actList.html", function (){
+                
+                        $("#btn1").click(function addAct(){
+                                var excerice = $('#userExercise').val();
+                                var amount = $('#userAmount').val();
+                            $("#activityList ul").append("<li class='list-group-item' >"+excerice+" , "+amount+"</li>");
+                        })
+        }
+)}
+)
